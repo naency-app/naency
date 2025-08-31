ac7d2d4f1e6a393b50b3a12825e2dec7a9f5a510
Danilo Miranda <danilomirandaa.15@outlook.com>
2025-08-31 11:13:03 -0400

    Add analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,15 @@
 'use client';
 
+import { Analytics } from '@vercel/analytics/next';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from '@/components/ui/sonner';
 import { TRPCProvider } from '@/lib/trpc-provider';
-
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <TRPCProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         <Toaster />
+        <Analytics />
         {children}
       </ThemeProvider>
     </TRPCProvider>
