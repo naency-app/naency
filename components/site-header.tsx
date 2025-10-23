8d3ae970f03da138bdd72cb08e8b021a9c9ac53b
Danilo Miranda <danilomirandaa.15@outlook.com>
2025-10-23 18:52:47 -0400

    fix: remove padding from site header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,7 +9,7 @@ import { ModeToggle } from './mode-toggle-theme';
 export function SiteHeader() {
   const pathname = usePathname();
   return (
-    <header className="flex h-(--header-height) shrink-0 fix items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="flex py-2 shrink-0 fix items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-2 lg:gap-2 ">
         <SidebarTrigger />
         <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
